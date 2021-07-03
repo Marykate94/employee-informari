ad21a9640a06251bb9de1c8e6ad1a234615cd761
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-03 11:53:11 -0400

    updated database name

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
   user: 'root',
   // Your MySQL password
   password: '#Brooklyn1950',
-  database: 'employee'
+  database: 'employeeDatabase'
 });
 
 module.exports = db;
