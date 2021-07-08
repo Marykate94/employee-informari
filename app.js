51662029a4cb068e9c8e9e9385b8441caea810a9
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-08 17:32:20 -0400

    added initial prompt to select what user wants to do

--- app.js
+++ app.js
@@ -20,6 +20,18 @@ function startPrompt() {
     inquirer.prompt([
         {
             // create prompts
+            type: "list",
+            message: "What would you like to do?",
+            name: "choice",
+            choices: [
+                "view all departments",
+                "view all roles",
+                "view all employees",
+                "add a department",
+                "add a role",
+                "add an employee",
+                "update and employee role"
+            ]
         }
     ])
 }
