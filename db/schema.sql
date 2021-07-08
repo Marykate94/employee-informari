0370747c5d22e149d5c2c15ecc737a28056dae56
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-07 22:41:20 -0400

    added all tables and fixed manager_id issue

--- db/schema.sql
+++ db/schema.sql
@@ -25,6 +25,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INTEGER,
     manager_id INTEGER,
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
     FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -18,19 +18,19 @@ VALUES
     ('Warehouse Foreman', 95000, 4),
     ('Warehouse Staff', 52500, 4);
 
-INSERT INTO employee (first_name, last_name, role_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Phyllis', 'Vance', 11),
-    ('Malcolm', 'Ex', 12),
-    ('Clark', 'Green', 12),
-    ('Danny', 'Cordray', 13),
-    ('Oscar', 'Martinez', 14),
-    ('Dakota', 'Fann', 15),
-    ('Pete', 'Miller', 16),
-    ('Meredith', 'Palmer', 17),
-    ('Devon', 'White', 18),
-    ('Val', 'Johnson', 19),
-    ('Nate', 'Smith', 20),
-    ('Glenn', 'Dora', 20),
-    ('Matt', 'Howell', 20),
-    ('Philip', 'Johnson', 20);
+    ('Phyllis', 'Vance', 11, null),
+    ('Malcolm', 'Ex', 12, 1),
+    ('Clark', 'Green', 12, 1),
+    ('Danny', 'Cordray', 13, 1),
+    ('Oscar', 'Martinez', 14, null),
+    ('Dakota', 'Fann', 15, 5),
+    ('Pete', 'Miller', 16, null),
+    ('Meredith', 'Palmer', 17, null),
+    ('Devon', 'White', 18, null),
+    ('Val', 'Johnson', 19, null),
+    ('Nate', 'Smith', 20, 10),
+    ('Glenn', 'Dora', 20, 10),
+    ('Matt', 'Howell', 20, 10),
+    ('Philip', 'Johnson', 20, 10);
