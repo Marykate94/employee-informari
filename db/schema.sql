75551974e8d124e82f768575b1320ce97839da15
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-07 22:35:55 -0400

    added all three tables to database - have to connect manager id to employees

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+-- drop table if exists section
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
 -- create department table 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY, 
@@ -5,6 +10,21 @@ CREATE TABLE department (
 );
 
 -- create role table
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
 
-
+-- create employee table 
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,33 @@ VALUES
     ('Accounting'),
     ('Product Oversight'),
     ('Warehouse');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Regional Director of Sales', 95000, 1),
+    ('Sales Rep.', 65000, 1),
+    ('Traveling Sales Rep.', 85000, 1),
+    ('Chief Accountant', 95000, 2),
+    ('Accountant', 82000, 2),
+    ('Customer Service Rep.', 50000, 3),
+    ('Supplier Relations Rep.', 55000, 3),
+    ('Quality Assurance Rep.', 52000, 3),
+    ('Warehouse Foreman', 95000, 4),
+    ('Warehouse Staff', 52500, 4);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES
+    ('Phyllis', 'Vance', 11),
+    ('Malcolm', 'Ex', 12),
+    ('Clark', 'Green', 12),
+    ('Danny', 'Cordray', 13),
+    ('Oscar', 'Martinez', 14),
+    ('Dakota', 'Fann', 15),
+    ('Pete', 'Miller', 16),
+    ('Meredith', 'Palmer', 17),
+    ('Devon', 'White', 18),
+    ('Val', 'Johnson', 19),
+    ('Nate', 'Smith', 20),
+    ('Glenn', 'Dora', 20),
+    ('Matt', 'Howell', 20),
+    ('Philip', 'Johnson', 20);
